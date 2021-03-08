05e7420b27cc7e8faf3a5c5baeb9197335c4f826
Dalton Okechukwu [terracotta55] <chike911@yahoo.com>
2021-03-07 18:46:55 -0700

    setting up mongoose model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/tasks-mongoose", {
     useNewUrlParser: true,
@@ -10,9 +11,39 @@ mongoose.connect("mongodb://127.0.0.1:27017/tasks-mongoose", {
 const User = mongoose.model("User", {
     name: {
         type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
     },
     age: {
         type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('age must be a positive number')
+            }
+        }
     },
 });
 
@@ -43,15 +74,15 @@ const Task = mongoose.model("Task", {
 //     { name: "Dwayne Wade", age: 35 }
 // );
 
-const task = new Task({
-    description: "water the plants",
-    completed: false,
-});
+// const task = new Task({
+//     description: "water the plants",
+//     completed: false,
+// });
 
-task.save()
-    .then((res) => {
-        console.log(res);
-    })
-    .catch((err) => {
-        console.log("error msg: ", err);
-    });
+// task.save()
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log("error msg: ", err);
+//     });
