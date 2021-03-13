d604ab274647ada0a230cd1ae914fffb54e5f176
Dalton Okechukwu [terracotta55] <chike911@yahoo.com>
2021-03-13 13:05:23 -0700

    adding timestamps to taskSchema and userSchema

--- src/models/user.js
+++ src/models/user.js
@@ -4,52 +4,57 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Task = require("./task");
 
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error("Email is invalid");
-            }
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
         },
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes("password")) {
-                throw new Error('Password cannot contain "password"');
-            }
-        },
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error("age must be a positive number");
-            }
-        },
-    },
-    tokens: [
-        {
-            token: {
-                type: String,
-                required: true,
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+            lowercase: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error("Email is invalid");
+                }
             },
         },
-    ],
-});
+        password: {
+            type: String,
+            required: true,
+            minlength: 7,
+            trim: true,
+            validate(value) {
+                if (value.toLowerCase().includes("password")) {
+                    throw new Error('Password cannot contain "password"');
+                }
+            },
+        },
+        age: {
+            type: Number,
+            default: 0,
+            validate(value) {
+                if (value < 0) {
+                    throw new Error("age must be a positive number");
+                }
+            },
+        },
+        tokens: [
+            {
+                token: {
+                    type: String,
+                    required: true,
+                },
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
 
 // set up virtual property (relationship b/w two collections)
 userSchema.virtual('tasks', {
