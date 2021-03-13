eaee131b0085ad972e81764f806b0c0878e24916
Dalton Okechukwu [terracotta55] <chike911@yahoo.com>
2021-03-13 14:26:56 -0700

    filtering, paginating and sorting task data

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,10 +19,35 @@ router.post("/tasks", auth, async (req, res) => {
     }
 });
 
-// get all current user's tasks
+// get all authenticated user's tasks
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=0
+// GET /tasks?sortBy=createdAt:desc or /tasks?sortBy=completed:desc
 router.get("/tasks", auth, async (req, res) => {
+    const match = {};
+    const sort = {};
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === "true"; // to boolean
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(":");
+        sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
+    }
+    
     try {
-        await req.user.populate("tasks").execPopulate();
+        await req.user
+            .populate({
+                path: "tasks",
+                match,
+                options: {
+                    limit: parseInt(req.query.limit), // to integer
+                    skip: parseInt(req.query.skip),
+                    sort,
+                },
+            })
+            .execPopulate();
         res.status(200).send(req.user.tasks);
     } catch (err) {
         res.status(500).send();
